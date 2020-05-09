817fc7d8f241217c1003aafdf28cd9e7c0e98397
Nikola Lazovic <nikolalazovic@tissaro.com>
2020-05-08 23:17:16 -0400

    updated for heroku db

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,23 @@ let connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "nik1",
-    password: "Welcome",
-    database: "burgers_db"
-  });
+  //when used on local machine
+  // connection = mysql.createConnection({
+  //   host: "localhost",
+  //   port: 3306,
+  //   user: "nik1",
+  //   password: "Welcome",
+  //   database: "burgers_db"
+  // });
 
+  //when used on heroku
+  connection = mysql.createConnection({
+    host: "us-cdbr-east-06.cleardb.net",
+    port: 3306,
+    user: "ba9b03db7fbd68",
+    password: "eb46dbb9",
+    database: "heroku_aad9294ba946023"
+  });
 }
 
 connection.connect(function(err) {
